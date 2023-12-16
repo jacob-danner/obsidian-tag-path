5e6902a43f99e1dd04659f1da0f0eb1bde3b8e88
Jacob Danner <jacob.danner@drake.edu>
2023-12-16 17:27:17 -0600

    init git

---
+++ main.ts
@@ -0,0 +1,16 @@
+import {
+    App,
+    Editor,
+    MarkdownView,
+    Modal,
+    Notice,
+    Plugin,
+    PluginSettingTab,
+    Setting,
+} from 'obsidian'
+
+export default class MyPlugin extends Plugin {
+    async onload() {}
+
+    async onunload() {}
+}
